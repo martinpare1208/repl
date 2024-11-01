8a2da1db645946c1d2bdd4a01e9746c1e74e5f95
martinpare1208 <martinapilado@gmail.com>
2024-11-01 15:34:57 -0400

    implemented logic to execute commands, next will be refactoring logic to handle more commands and better error messages

--- commands/commands.go
+++ commands/commands.go
@@ -4,22 +4,45 @@ import(
 	"fmt"
 )
 
-type cliCommand struct {
-	name string
-	desc string
-	callback func() error
+type CliCommand struct {
+	Name string
+	Desc string
+	Callback func() error
 }
 
-var CommandsList = map[string]cliCommand{
+type WrongCommandError struct {
+	Command string
+}
+
+var CommandsList = map[string]CliCommand{
+
 	"help": {
-		name: "help",
-		desc: "display commands available",
-		callback: getHelp,
+		Name: "help",
+		Desc: "display commands available",
+		Callback: getHelp,
 	},
 
 }
 
-func getHelp() error {
+func(c WrongCommandError) Error() string {
+	return fmt.Sprintf("command: '%s' not found: ", c.Command)
+}
+
+func getHelp() (error) {
+
 	fmt.Println("Help successful")
 	return nil
+}
+
+func ReadCommand(command string) error {
+	for c := range CommandsList {
+		if command == c {
+			err := CommandsList[command].Callback()
+			if err == nil {
+				return nil
+			}
+
+		}
+	}
+	return WrongCommandError{Command: command}
 }
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 import (
 	"fmt"
+	"github.com/martinpare1208/pokedexcli/commands"
 )
 
 func main() {
@@ -12,7 +13,10 @@ func main() {
 		
 		fmt.Print("pokedex > ")
 		fmt.Scanln(&command)
-
+		err := commands.ReadCommand(command)
+		if err == nil {
+			fmt.Println("Command executed!")
+		}
 
 	}
 	
